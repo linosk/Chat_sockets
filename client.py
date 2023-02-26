caf77c1b337acae3aa29720fd119436dd7cfb0bd
linosk <linakoskar@gmail.com>
2023-02-26 16:55:36 +0100

    No problems at the moment

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-#TODO make user choose port number and IP address
+#TODO make user choose port number, IP address, and maybe coding
 host = '127.0.0.1'
 port = 55555
+coding = "utf-8"
+#TODO - take care of cases when the buffer is overflown, maybe use header
+buffer = 1024
 
+nickname = input("Enter your nickname: ")
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((host,port))
+
+client_socket.send(nickname.encode(coding))
 
 print("Connection established.")
--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import socket
+import threading
 
 #host = socket.gethostname() 
 host = '127.0.0.1'
 #TODO - allow to choose port
 port = 55555
+#TODO - allow a server to change encoding - MAYBE
+coding = "utf-8"
+#TODO - take care of cases when the buffer is overflown, maybe use header
+buffer = 1024
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #TODO - allow given user to reconnect
@@ -12,7 +17,30 @@ server_socket.bind((host,port))
 #TODO - (maybe) pass value in listen
 server_socket.listen()
 
-print("Awaiting for connection...")
-server_socket.accept()
+def start():
+    clients = []
+    addresses = []
+    condition = True
 
-print("User connected.")
+    #TODO - function dedicated for commands from server, add other functions
+    def server_commands():
+        nonlocal condition
+        command = input("")
+        if command == "end":
+            print("Server forcefully terminated.")
+            condition = False
+            return
+
+    while condition:
+        print("Server started.")
+        if len(clients) == 0:
+            print("Awaiting for connections...")
+        t1 = threading.Thread(target=server_commands, args=())
+        t1.start()
+        #server_commands()
+        client_socket, client_address = server_socket.accept()
+        client_nickname = client_socket.recv(buffer).decode(coding)
+        print(client_nickname)
+        t1.join()
+
+start()
