870b8bea22b38b058717187b2c9ba3e3aa0fe204
linosk <linakoskar@gmail.com>
2023-02-26 14:30:21 +0100

    Starting from 0 to futhrer understanding

--- client.py
+++ client.py
@@ -1,34 +1,11 @@
 import socket
 
-connection_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-try:
-    connection_socket.connect((socket.gethostname(),8080))
-    condition = True
+#TODO make user choose port number and IP address
+host = '127.0.0.1'
+port = 55555
 
-    while condition:
-        msg = connection_socket.recv(1024)
-        print(msg.decode("utf-8"))
-        sendto = input("")
-        connection_socket.send(sendto.encode("utf-8"))
-        condition = False
+client_socket.connect((host,port))
 
-except:
-    print("Unable to establish connection with the server.")
-
-##Ip address of target computer and selected port
-#host_address = '192.168.0.207'
-#port = 60000
-#
-##Socket for communication
-#client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#
-##Connect to server
-#client_socket.connect((host_address,port))
-#
-##while True:
-#rx = client_socket.recv(1024).decode('utf-8')
-##message = client_socket.recv(1024).decode('utf-8')
-#print(rx+"\n")
-#
-#functions.comm_client(client_socket,'utf-8',1024)
+print("Connection established.")
--- server.py
+++ server.py
@@ -1,148 +1,18 @@
 import socket
-import threading
 
-#thread for receiving messages
-#thread for server command
+#host = socket.gethostname() 
+host = '127.0.0.1'
+#TODO - allow to choose port
+port = 55555
 
-#Maybe later make it an option to use switch between maybe ipv4/ipv6 and tcp/udp
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#TODO - allow given user to reconnect
+server_socket.bind((host,port))
 
-#Port selection?
-port = 8080
-server_socket.bind((socket.gethostname(),port))
+#TODO - (maybe) pass value in listen
+server_socket.listen()
 
-#What exacly is happening?
-server_socket.listen(1)
-print("Awaiting clients...")
+print("Awaiting for connection...")
+server_socket.accept()
 
-#List of connected clients and their nicknames, the same values with the same indexes correspond to the same client
-addresses = []
-nicknames = []
-
-condition = True
-
-def server_command():
-    command = input()
-    if(command=="end"):
-        condition = False
-    return condition
-
-def accept_client(server_socket,addresses,nicknames):
-    client_socket, client_address = server_socket.accept()
-    client_socket.send("You succesfully connected to the server. Who are you?".encode("utf-8"))
-    print(f"{client_address} connected to the server.")
-    addresses.append(client_address)
-    client_nickname = client_socket.recv(1024).decode("utf-8")
-    print(f"{client_address} nickname is: {client_nickname}")
-    nicknames.append(client_nickname)
-
-t1 = threading.Thread(target=accept_client, args=(server_socket,addresses,nicknames))
-#t2 = threading.Thread(target=server_command)
-
-semaphore = threading.Semaphore(1)
-
-while condition:
-    semaphore.acquire()
-    t1.start()
-
-server_socket.close()
-print("Server application terminated.")
-
-for i in range(len(nicknames)):
-    print(f"The user {nicknames[i]} connected from {addresses[i]}.")
-
-"""
-import socket
-import functions
-
-#Ip address of current computer and selected port
-host_address = '192.168.0.207'
-port = 8080
-
-#Socket for server(IPv4 and TCP)
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-#Binding socket
-server_socket.bind((host_address,port))
-
-#Listen for 1 connection
-server_socket.listen(1)
-
-#Accept connection
-client_socket, client_address = server_socket.accept()
-
-print("Connection established")
-client_socket.send("Connection established".encode('utf-8'))
-print("\n")
-
-functions.comm_server(client_socket,'utf-8',1024)
-
-s.py file
-
-import socket
-import time
-
-HEADERSIZE = 10
-
-#msg = "Welcome to the server!"
-#print(f'{len(msg):<{HEADERSIZE}}'+msg)
-
-#AF_INET - ipv4, SOCK_STREAM - tcp
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#gethostbyname() - bassiacly address 127.0.1.1
-s.bind((socket.gethostname(),60000))
-
-s.listen(5)
-
-while True:
-    clientsocket, address = s.accept()
-    print(f"{address} connected.")
-    msg = "Welcome to the server!"
-    msg = f'{len(msg):<{HEADERSIZE}}'+msg
-    clientsocket.send(msg.encode('ascii'))
-    #clientsocket.send(bytes("Hello","ascii"))
-    while True:
-        time.sleep(3)
-        msg = f"The rime is!{time.time()}"
-        msg = f'{len(msg):<{HEADERSIZE}}'+msg
-        clientsocket.send(msg.encode('ascii'))
-s.close()
-
-print(socket.gethostbyname("me-VirtualBox"))
-#print(socket.gethostbyaddr())
-#print(socket.gethostbyname_ex())
-#print(socket.gethostname())
-
-c.py file
-
-import socket
-
-HEADERSIZE = 10
-
-#AF_INET - ipv4, SOCK_STREAM - tcp
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-ip = '192.168.0.25'
-#only when there is only one machine
-s.connect((ip,60000))
-
-#Buffering is important
-while True:
-    fullmsg=''
-    newmsg=True
-    while True:
-        msg = s.recv(16)
-        if newmsg:
-            print(f"New message length: {msg[:HEADERSIZE]}")
-            msglen= int(msg[:HEADERSIZE])
-            newmsg = False
-        fullmsg+=msg.decode("ascii")
-        if len(fullmsg)-HEADERSIZE==msglen:
-            print("full msg recvd")
-            print(fullmsg[HEADERSIZE:])
-            newmsg=True
-            fullmsg = ''
-    print(fullmsg) 
-
-"""
+print("User connected.")
